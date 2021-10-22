1ecc00152082175510225fa8bd4e341a2938e358
rocky <rock9247@naver.com>
2021-10-22 09:59:21 -0400

    Firebase Auth

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,8 +3,7 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 
-export default () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
       <Switch>
@@ -23,3 +22,5 @@ export default () => {
     </Router>
   );
 };
+
+export default AppRouter;
--- src/fbase.js
+++ src/fbase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-app.js";
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,4 +18,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app;
+export const authService = getAuth();
